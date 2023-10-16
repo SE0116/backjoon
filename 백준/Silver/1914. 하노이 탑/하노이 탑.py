891141a3beaf048a61e26f3710e1369679cbfbc5
SE0116 <87646738+SE0116@users.noreply.github.com>
2023-10-16 22:10:49 +0900

    [Silver I] Title: 하노이 탑, Time: 976 ms, Memory: 105928 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.py
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -1,33 +1,20 @@
-input_circle_size = int(input())
+def move(no, x, y, path):
+    if no == 1:
+        path.append((x, y))
+    else:
+        move(no - 1, x, 6 - x - y, path)
+        if no <= 20:
+            path.append((x, y))
+        move(no - 1, 6 - x - y, y, path)
 
-def hanoi(size, start, end):
-  if size > 1:
-    hanoi(size-1, start, 6-start-end)
-  
-  global move
-  move += 1
-  
-  if input_circle_size <= 20:
-    global from_start, to_end
-    from_start += [start]
-    to_end += [end]
-
-
-  if size > 1:
-    hanoi(size-1, 6-start-end, end)
-
-
-if input_circle_size > 20:
-  print(2**input_circle_size - 1)
+n = int(input())
+if n > 20:
+    print(2**n-1)
 else:
-  global move, from_start, to_end
-  move, from_start, to_end = 0, [], []
+  move_path = []
+  move(n, 1, 3, move_path)
 
-
-
-  hanoi(input_circle_size, 1, 3)
-
-  print(move)
-  if input_circle_size <= 20:
-    for idx in range(move):
-      print(from_start[idx], to_end[idx])
+  print(len(move_path))
+  if n <= 20:
+      for move_step in move_path:
+          print(f'{move_step[0]} {move_step[1]}')
