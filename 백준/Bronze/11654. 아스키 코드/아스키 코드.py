b0eff9890195ad1976cd541f86381ce544d86fa3
SE0116 <87646738+SE0116@users.noreply.github.com>
2023-10-16 21:17:53 +0900

    [Bronze V] Title: 아스키 코드, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.py
@@ -0,0 +1,12 @@
+inputData = input()
+
+alphaList = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+  'A', 'B', 'C' ,'D', 'E', 'F', 'G', 'H' ,'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+
+if inputData in alphaList:
+  print(ord(inputData))
+else:
+  inputData = int(inputData)
+  print(int(inputData)+48)
