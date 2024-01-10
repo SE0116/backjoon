98c790c35a0394c5ce03f169bc59cb40631894fd
SE0116 <87646738+SE0116@users.noreply.github.com>
2024-01-10 16:28:18 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,12 @@
+function solution(brown, yellow) {
+    let answer = [];
+    for ( let i = 3 ; i <= brown + yellow / i ; i++ ) {
+        let x = Math.floor((brown + yellow) / i);
+        if ( (x-2) * (i-2) === yellow) {
+            answer.push(x);
+            answer.push(i);
+            break;
+        }
+    }
+    return answer;
+}
