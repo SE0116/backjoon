affa0b2bc2bfd667d2bba30fad0b9de0428f731d
SE0116 <87646738+SE0116@users.noreply.github.com>
2024-05-13 22:32:22 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.94 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = 0;
+    const dp = Array(n).fill(0);
+    
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for ( let i = 3 ; i <= n ; i++ ) {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+    }
+    
+    return dp[n];
+}
